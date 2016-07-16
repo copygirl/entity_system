c2af4dae38fd14468f54f7168a4a7fffadfb58c5
copygirl <halfnerd.copygirl@gmail.com>
2016-07-16 13:19:59 +0200

    Update test to use custom struct types

--- tests/lib.rs
+++ tests/lib.rs
@@ -2,20 +2,25 @@ extern crate entity_system;
 
 use entity_system::*;
 
+#[derive(PartialEq, Clone, Copy, Debug)]
+struct Location(f32, f32, f32);
+
+#[derive(PartialEq, Eq, Clone, Copy, Debug)]
+struct Health(i32);
+
 
 #[test]
 fn test() {
-    let entity   = Entity(0);
-    let location = [ 0f32; 3 ];
-    
     let mut ec_map = storage::Map::<Entity>::new();
+    
+    let entity   = Entity(0);
+    let location = Location(1.0, 2.0, 3.0);
+    
     ec_map.insert(entity, location);
+    assert_eq!(ec_map.borrow::<Location>(entity), Some(&Location(1.0, 2.0, 3.0)));
+    assert_eq!(ec_map.get::<Location>(entity), Some(Location(1.0, 2.0, 3.0)));
+    assert_eq!(ec_map.get::<Health>(entity), None);
     
-    assert_eq!(ec_map.borrow::<[f32;3]>(entity), Some(&[ 0f32; 3 ]));
-    assert_eq!(ec_map.get::<[f32;3]>(entity), Some([ 0f32; 3 ]));
-    assert_eq!(ec_map.get::<[f32;2]>(entity), None);
-    
-    ec_map.remove::<[f32;3]>(entity);
-    
-    assert_eq!(ec_map.get::<[f32;3]>(entity), None);
+    ec_map.remove::<Location>(entity);
+    assert_eq!(ec_map.get::<Location>(entity), None);
 }
