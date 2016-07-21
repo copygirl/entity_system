0bea945c4f38cf9257d90712921e71058000e883
copygirl <halfnerd.copygirl@gmail.com>
2016-07-21 17:55:27 +0200

    Remove (currently) Clone requirement from Entity

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,8 @@ use std::hash::Hash;
 pub mod storage;
 
 
-pub trait Entity: Any + Eq + Hash + Clone { }
-impl<T: Any + Eq + Hash + Clone> Entity for T { }
+pub trait Entity: Any + Eq + Hash { }
+impl<T: Any + Eq + Hash> Entity for T { }
 
 pub trait Component: Any { }
 impl<T: Any> Component for T { }
