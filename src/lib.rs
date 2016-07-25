61cd6a6975f0277656d76a9ae3a9ad580730a14f
copygirl <halfnerd.copygirl@gmail.com>
2016-07-25 11:36:02 +0200

    Require Default trait on Entity

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,8 @@ use std::hash::Hash;
 pub mod storage;
 
 
-pub trait Entity: Any + Eq + Hash { }
-impl<T: Any + Eq + Hash> Entity for T { }
+pub trait Entity: Any + Eq + Hash + Default { }
+impl<T: Any + Eq + Hash + Default> Entity for T { }
 
 pub trait Component: Any { }
 impl<T: Any> Component for T { }
